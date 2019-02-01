6b0edaa1e1b45c52980db5e4a7db9cb50d94e53d
Arie May Wibowo <ariemayw@gmail.com>
2019-02-02 01:25:59 +0700

    add code to get image from url

--- lib/hal_radio.dart
+++ lib/hal_radio.dart
@@ -10,7 +10,9 @@ class Radio extends StatelessWidget {
             new Padding(padding: EdgeInsets.all(20.0),),
             new Text("RADIO", style: new TextStyle(fontSize: 30.0),),
             new Padding(padding: EdgeInsets.all(20.0),),
-            new Image.asset("img/radio.jpg", width: 200.0,)
+            new Image.asset("img/radio.jpg", width: 200.0,),
+            new Padding(padding: EdgeInsets.all(20.0),),
+            new Image(image: new NetworkImage("https://images-na.ssl-images-amazon.com/images/I/91jmKEUsVuL._SX466_.jpg"), width: 200.0,)
           ],
         ),
       ),
